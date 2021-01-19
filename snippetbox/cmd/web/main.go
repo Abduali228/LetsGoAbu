5e7f8bead561e8b2eb1dcf8bdafdcf0f4a03f057
Abduali228 <54325881+Abduali228@users.noreply.github.com>
2021-01-19 21:41:39 +0600

    Add files via upload
    Я создал новый репозиторий для мр.Тлеужана потому что с прошлой появились затруднения. Заранее извиняюсь за неудобства.

---
+++ snippetbox/cmd/web/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"github.com/jackc/pgx/v4/pgxpool"
+	"log"
+	"net/http"
+	"os"
+	"snippetbox/pkg/models/postgres"
+)
+
+const GetById = "SELECT id, name FROM person where id=$1"
+
+type application struct {
+	errorLog *log.Logger
+	infoLog  *log.Logger
+	snippet *postgres.SnippetModel
+}
+
+func main() {
+	addr := flag.String("addr", ":4000", "HTTP network address")
+	flag.Parse()
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+	pool, err := pgxpool.Connect(context.Background(), "user=postgres password=123 host=localhost port=5432 dbname=postgres sslmode=disable pool_max_conns=10")
+	if err != nil {
+		log.Fatalf("Unable to connection to database: %v\n", err)
+	}
+
+	defer pool.Close()
+
+	app := &application{
+		errorLog: errorLog,
+		infoLog:  infoLog,
+		snippet: &postgres.SnippetModel{Pool: pool},
+	}
+	srv := &http.Server{
+		Addr:     *addr,
+		ErrorLog: errorLog,
+		Handler:  app.routes(),
+	}
+	infoLog.Printf("Starting server on %s", *addr)
+	err = srv.ListenAndServe()
+	errorLog.Fatal(err)
+}
